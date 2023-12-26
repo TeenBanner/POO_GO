315c709811f6da838425040a302b4579b89377e1
Alfonso <alfonso.dancerod@gmail.com>
2023-12-26 15:22:59 -0600

    nada

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
-func main(){
-  fmt.Println("Hola mundo")
+func main() {
+	go loop(0, 1000000)
+	for i := 0; i < 1000000; i++ {
+		fmt.Println("proceso main iteracion numero: ", i)
+	}
+}
+
+func loop(n, n2 int) {
+	valor_usuario := n2
+
+	for i := n; i < valor_usuario; i++ {
+		fmt.Println("proceso 2 iteracion: ", i)
+	}
 }
